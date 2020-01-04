01b0cd3c19086763cd98c8f9a3b000c740609190
Afrosofttech <contact@afrosofttech.com>
2020-01-04 11:38:13 +0530

    amadou

---
+++ post.php
@@ -0,0 +1,55 @@
+<?php 
+// For all post methods or queries that involve inserting, creating, updating
+// and deleting something on the database, direct all the URL\'s here and then 
+// reroute them to the correct controller and method. I know that all this is not neccessary 
+// if we were only interacting with the controllers and never with the views but if we are 
+// to use the first MVC model, then there will be no need for views here since Jquery will handle everything involving viewing
+
+$uri = $_SERVER['REQUEST_URI'];
+$urlParts = parse_url($uri, PHP_URL_PATH);
+$urlParts = explode('/',filter_var(rtrim($urlParts, '/'), FILTER_SANITIZE_URL));
+
+ $method =  $urlParts[sizeof($urlParts)-1];
+ $contr =  $urlParts[sizeof($urlParts)-2];
+
+ if($contr == 'company'){
+    require_once('controller/companycontroller.php');
+    switch ($method){
+        case "":
+           ;
+           break;
+       case "":
+           ;
+           break;
+        default:
+           break;
+    }
+
+ } elseif($contr == 'jobseeker'){
+    require_once('controller/jobseekercontroller.php');
+    switch ($method){
+        case "":
+           ;
+           break;
+       case "":
+           ;
+           break;
+        default:
+           break;
+    }
+
+ }else{
+    require_once('controller/admincontroller.php');
+    switch ($method){
+        case "":
+           ;
+           break;
+       case "":
+           ;
+           break;
+        default:
+           break;
+    }
+ }
+
+?>
