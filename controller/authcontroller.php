5cbbb4c9d34f0aa44c94bf04e1fb44b916ab9e78
techjobis <biranjobe69@gmail.com>
2020-01-29 21:48:37 +0100

    usertype session variable modified

--- controller/authcontroller.php
+++ controller/authcontroller.php
@@ -38,12 +38,12 @@ class AuthController extends Auth{
            if($user['user_type'] == 'company'){
               $company = $this->get_company_login($user['login_id']);
               $_SESSION['name'] = $company['company_name'];
-              $_SESSION['uertype'] = $user['user_type'];
+              $_SESSION['usertype'] = $user['user_type'];
            }
            if($user['user_type'] == 'jobseeker'){
               $jobseeker = $this->get_jobseeker_login($user['login_id']);
               $_SESSION['name'] = $jobseeker['fullname'];
-              $_SESSION['uertype'] = $user['user_type'];
+              $_SESSION['usertype'] = $user['user_type'];
            }
            return true;
         } else {
