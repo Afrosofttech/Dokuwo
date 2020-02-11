7cab04cec55d81a96be8545160592a1107d36c61
Amadou Sarjo Jallow <asj.sarjo@gmail.com>
2020-02-12 03:50:14 +0530

    Update company.js

--- js/company.js
+++ js/company.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    loadCompanyDashboard();
+//
 })
 function sideBar(){
         return '<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">'+
@@ -196,7 +196,6 @@ function topBar(){
 
     '</nav>'; 
 }
-<<<<<<< HEAD
 function alertNotification(){
   //AMS:not being used at the moment
   return '<li class="nav-item dropdown no-arrow mx-1">'+
@@ -318,101 +317,40 @@ function dashBoardContentheader(){
             '<div class="col mr-2">'+
               '<div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Jobs Published</div>'+
               '<div class="h5 mb-0 font-weight-bold text-gray-800" id="jobsPublished">0<script>number_of_jobs("jobsPublished")</script></div>'+
-=======
-function dashBoardContent(){
-    return '<div class="container-fluid">'+
-
-      '<div class="row">'+
-
-        '<div class="col-xl-4 col-md-6 mb-4">'+
-          '<div class="card border-left-primary shadow h-100 py-2">'+
-            '<div class="card-body">'+
-              '<div class="row no-gutters align-items-center">'+
-                '<div class="col mr-2">'+
-                  '<div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Jobs Published</div>'+
-                  '<div class="h5 mb-0 font-weight-bold text-gray-800">0</div>'+
-                '</div>'+
-                '<div class="col-auto">'+
-                  '<i class="fas fa-briefcase fa-2x text-gray-300"></i>'+
-                '</div>'+
-              '</div>'+
->>>>>>> origin/Signup_review
             '</div>'+
-          '</div>'+
-        '</div>'+
-
-        '<div class="col-xl-4  col-md-6 mb-4">'+
-          '<div class="card border-left-info shadow h-100 py-2">'+
-            '<div class="card-body">'+
-              '<div class="row no-gutters align-items-center">'+
-                '<div class="col mr-2">'+
-                  '<div class="text-xs font-weight-bold text-info text-uppercase mb-1">Profile(Completion)</div>'+
-                  '<div class="row no-gutters align-items-center">'+
-                    '<div class="col-auto">'+
-                      '<div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">50%</div>'+
-                    '</div>'+
-                    '<div class="col">'+
-                      '<div class="progress progress-sm mr-2">'+
-                        '<div class="progress-bar bg-info" role="progressbar" style="width: 50%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>'+
-                      '</div>'+
-                    '</div>'+
-                  '</div>'+
-                '</div>'+
-                '<div class="col-auto">'+
-                  '<i class="fas fa-user fa-2x text-gray-300"></i>'+
-                '</div>'+
-              '</div>'+
-            '</div>'+
-          '</div>'+
-        '</div>'+
-
-        '<div class="col-xl-4 col-md-6 mb-4">'+
-          '<div class="card border-left-warning shadow h-100 py-2">'+
-            '<div class="card-body">'+
-              '<div class="row no-gutters align-items-center">'+
-                '<div class="col mr-2">'+
-                  '<div class="text-xs font-weight-bold text-warning text-uppercase mb-1">Messages</div>'+
-                  '<div class="h5 mb-0 font-weight-bold text-gray-800">5</div>'+
-                '</div>'+
-                '<div class="col-auto">'+
-                  '<i class="fas fa-comments fa-2x text-gray-300"></i>'+
-                '</div>'+
-              '</div>'+
+            '<div class="col-auto">'+
+              '<i class="fas fa-briefcase fa-2x text-gray-300"></i>'+
             '</div>'+
           '</div>'+
         '</div>'+
       '</div>'+
+    '</div>'+
 
-
-      '<div class="row">'+
-
-        '<div class="col-xl-8 col-lg-7">'+
-          '<div class="card shadow mb-4">'+
- 
-            '<div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">'+
-              '<h6 class="m-0 font-weight-bold text-primary">Earnings Overview</h6>'+
-              '<div class="dropdown no-arrow">'+
-                '<a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">'+
-                  '<i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>'+
-                '</a>'+
-                '<div class="dropdown-menu dropdown-menu-right shadow animated--fade-in" aria-labelledby="dropdownMenuLink">'+
-                  '<div class="dropdown-header">Dropdown Header:</div>'+
-                  '<a class="dropdown-item" href="#">Action</a>'+
-                  '<a class="dropdown-item" href="#">Another action</a>'+
-                  '<div class="dropdown-divider"></div>'+
-                  '<a class="dropdown-item" href="#">Something else here</a>'+
+    '<div class="col-xl-4  col-md-6 mb-4">'+
+      '<div class="card border-left-info shadow h-100 py-2">'+
+        '<div class="card-body">'+
+          '<div class="row no-gutters align-items-center">'+
+            '<div class="col mr-2">'+
+              '<div class="text-xs font-weight-bold text-info text-uppercase mb-1">Profile(Completion)</div>'+
+              '<div class="row no-gutters align-items-center">'+
+                '<div class="col-auto">'+
+                  '<div class="h5 mb-0 mr-3 font-weight-bold text-gray-800" id="profileVal">loading..</div>'+
                 '</div>'+
+                '<div class="col">'+
+                  '<div class="progress progress-sm mr-2">'+
+                    '<div class="progress-bar bg-info" role="progressbar" style="width: 50%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" id="profileBar"></div>'+
+                  '</div>'+
+                '</div>'+
+                '<script>'+
+                'profile_completion("profileVal","profileBar")'+
+                '</script>'+
               '</div>'+
             '</div>'+
-
-            '<div class="card-body">'+
-              '<div class="chart-area">'+
-                '<canvas id="myAreaChart"></canvas>'+
-              '</div>'+
+            '<div class="col-auto">'+
+              '<i class="fas fa-user fa-2x text-gray-300"></i>'+
             '</div>'+
           '</div>'+
         '</div>'+
-<<<<<<< HEAD
       '</div>'+
     '</div>'+
 
@@ -423,102 +361,18 @@ function dashBoardContent(){
             '<div class="col mr-2">'+
               '<div class="text-xs font-weight-bold text-warning text-uppercase mb-1">Job Seekers</div>'+
               '<div class="h5 mb-0 font-weight-bold text-gray-800" id="noOfJobSeekers">0</div>'+
-=======
-
- 
-        '<div class="col-xl-4 col-lg-5">'+
-          '<div class="card shadow mb-4">'+
-
-            '<div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">'+
-              '<h6 class="m-0 font-weight-bold text-primary">Revenue Sources</h6>'+
-             '<div class="dropdown no-arrow">'+
-                '<a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">'+
-                  '<i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>'+
-                '</a>'+
-                '<div class="dropdown-menu dropdown-menu-right shadow animated--fade-in" aria-labelledby="dropdownMenuLink">'+
-                  '<div class="dropdown-header">Dropdown Header:</div>'+
-                  '<a class="dropdown-item" href="#">Action</a>'+
-                  '<a class="dropdown-item" href="#">Another action</a>'+
-                  '<div class="dropdown-divider"></div>'+
-                  '<a class="dropdown-item" href="#">Something else here</a>'+
-                '</div>'+
-             '</div>'+
->>>>>>> origin/Signup_review
             '</div>'+
-
-            '<div class="card-body">'+
-              '<div class="chart-pie pt-4 pb-2">'+
-                '<canvas id="myPieChart"></canvas>'+
-              '</div>'+
-              '<div class="mt-4 text-center small">'+
-                '<span class="mr-2">'+
-                  '<i class="fas fa-circle text-primary"></i> Direct'+
-                '</span>'+
-                '<span class="mr-2">'+
-                  '<i class="fas fa-circle text-success"></i> Social'+
-                '</span>'+
-                '<span class="mr-2">'+
-                  '<i class="fas fa-circle text-info"></i> Referral'+
-                '</span>'+
-              '</div>'+
+            '<div class="col-auto">'+
+              '<i class="fas fa-comments fa-2x text-gray-300"></i>'+
             '</div>'+
           '</div>'+
         '</div>'+
       '</div>'+
+    '</div>'+
+  '</div>'+
 
-      '<div class="row">'+
+'</div>';
 
-        '<div class="col-lg-6 mb-4">'+
-
-          '<div class="card shadow mb-4">'+
-            '<div class="card-header py-3">'+
-              '<h6 class="m-0 font-weight-bold text-primary">Projects</h6>'+
-            '</div>'+
-            '<div class="card-body">'+
-              '<h4 class="small font-weight-bold">Server Migration <span class="float-right">20%</span></h4>'+
-              '<div class="progress mb-4">'+
-                '<div class="progress-bar bg-danger" role="progressbar" style="width: 20%" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100"></div>'+
-              '</div>'+
-              '<h4 class="small font-weight-bold">Sales Tracking <span class="float-right">40%</span></h4>'+
-              '<div class="progress mb-4">'+
-                '<div class="progress-bar bg-warning" role="progressbar" style="width: 40%" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100"></div>'+
-              '</div>'+
-              '<h4 class="small font-weight-bold">Customer Database <span class="float-right">60%</span></h4>'+
-              '<div class="progress mb-4">'+
-                '<div class="progress-bar" role="progressbar" style="width: 60%" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100"></div>'+
-              '</div>'+
-              '<h4 class="small font-weight-bold">Payout Details <span class="float-right">80%</span></h4>'+
-              '<div class="progress mb-4">'+
-                '<div class="progress-bar bg-info" role="progressbar" style="width: 80%" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>'+
-              '</div>'+
-              '<h4 class="small font-weight-bold">Account Setup <span class="float-right">Complete!</span></h4>'+
-              '<div class="progress">'+
-               '<div class="progress-bar bg-success" role="progressbar" style="width: 100%" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>'+
-              '</div>'+
-            '</div>'+
-          '</div>'+
-
-        '</div>'+
-
-        '<div class="col-lg-6 mb-4">'+
-
-          '<div class="card shadow mb-4">'+
-           '<div class="card-header py-3">'+
-              '<h6 class="m-0 font-weight-bold text-primary">Illustrations</h6>'+
-            '</div>'+
-            '<div class="card-body">'+
-              '<div class="text-center">'+
-                '<img class="img-fluid px-3 px-sm-4 mt-3 mb-4" style="width: 25rem;" src="img/undraw_posting_photo.svg" alt="">'+
-              '</div>'+
-              '<p>Add some quality, svg illustrations to your project courtesy of <a target="_blank" rel="nofollow" href="https://undraw.co/">unDraw</a>, a constantly updated collection of beautiful svg images that you can use completely free and without attribution!</p>'+
-              '<a target="_blank" rel="nofollow" href="https://undraw.co/">Browse Illustrations on unDraw &rarr;</a>'+
-            '</div>'+
-          '</div>'+
-
-        '</div>'+
-      '</div>'+
-
-    '</div>';
 }
 function loadCompanyDashboard(){
  
@@ -526,13 +380,12 @@ function loadCompanyDashboard(){
    //let topbar = topBar();
    $('#wrapper').prepend(sidebar);
    let topbar = topBar();
-   let dbcontent = dashBoardContent();
+   let dbcontentHeader = dashBoardContentheader();
    let foot = footer();
 
   $('#content-wrapper').prepend(topbar);
-  $('#content').append(dbcontent);
+  $('#content').append(dbcontentHeader);
   $('#content-wrapper').append(foot);
-<<<<<<< HEAD
   
   $.notify("Welcome back", 'success');
   
@@ -560,11 +413,21 @@ function number_of_jobs(e){
     }
    });
 
-=======
->>>>>>> origin/Signup_review
 }
+function profile_completion(id1,id2){
+  $.ajax({
+    method: "GET",
+    url: "get.php/company/is_profile_complete",
+    data: {"login_id" : session_id},
+    success: function(data){
+     $('#'+id1 ).html(data+"%");
+     $('#'+id2).css('width',''+data+'%');
+   },
+   error: function(err){
+    //to be included soon
+   }
+  });
 
-<<<<<<< HEAD
 }
 function no_of_job_seekers(){
   $.ajax({
@@ -583,11 +446,13 @@ function no_of_job_seekers(){
 
 }
 function dashBoardContent(){
-=======
->>>>>>> origin/Signup_review
 
+  let temp ='<div class="container-fluid"><div class="row dbInner"></div></div>';
 
-<<<<<<< HEAD
+  $('#content').append(temp);
+  dashboardProfile();
+}
+
 function dashboardProfile(){
 
 let profile = '';
@@ -1855,5 +1720,3 @@ let profile =
 
   $('.profileInner').empty().append(profile);
 }
-=======
->>>>>>> origin/Signup_review
