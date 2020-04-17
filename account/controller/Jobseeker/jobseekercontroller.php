36ac1e427a1e4bbc04bfcd809f7fbe013beb449e
AMS-JR <asj.sarjo@gmail.com>
2020-04-18 04:55:57 +0530

    package
    freelancer packaging

--- account/controller/Jobseeker/jobseekercontroller.php
+++ account/controller/Jobseeker/jobseekercontroller.php
@@ -7,4 +7,35 @@ class JobseekerController extends Jobseeker{
        $res = $this->delete_this_hire($hire_id);   
        return $res;
     }
+    public function request_to_activate_package($login_id,$package){
+        $res = $this->requesting_this_package($login_id,$package);   
+        return $res;
+    }
+    public function add_service($jobseeker_id,$price,$name){
+        $res = $this->add_this_service($jobseeker_id,$price,$name);   
+        return $res;
+    }
+    public function update_service($service_id,$price,$name){
+        $res = $this->update_this_service($service_id,$price,$name);   
+        return $res;
+    }
+    public function delete_service($service_id)
+    {
+        $res = $this->delete_this_service($service_id);   
+        return $res;
+    }
+    public function add_portfolio($jobseeker_id,$url_link,$description,$type)
+    {
+        $res = $this->add_this_portfolio($jobseeker_id,$url_link,$description,$type);   
+        return $res;
+    }
+    public function update_portfolio($portfolio_id,$url_link,$description,$type)
+    {
+        $res = $this->update_this_portfolio($portfolio_id,$url_link,$description,$type);  
+        return $res;
+    }
+    public function delete_portfolio($portfolio_id){
+        $res = $this->delete_this_portfolio($portfolio_id);   
+        return $res;
+    }
 }
