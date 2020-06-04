aa09b8d40295634d4d970cbdf86fc03a9702c9f9
Amadou Sarjo Jallow <asj.sarjo@gmail.com>
2020-06-04 23:55:44 +0000

    Update post.php

--- account/post.php
+++ account/post.php
@@ -6,6 +6,7 @@ include 'includes/autoloader.inc.php';
 // if we were only interacting with the controllers and never with the views but if we are 
 // to use the first MVC model, then there will be no need for views here since Jquery will handle everything involving viewing
 $uri = $_SERVER['REQUEST_URI'];
+var_dump($uri);
 $urlParts = parse_url($uri, PHP_URL_PATH);
 $urlParts = explode('/',filter_var(rtrim($urlParts, '/'), FILTER_SANITIZE_URL));
 
