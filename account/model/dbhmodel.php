5356d148b95da0e5d636105187de668ff997980f
Amadou Sarjo Jallow <asj.sarjo@gmail.com>
2020-06-04 20:40:21 +0000

    Update dbhmodel.php

--- account/model/dbhmodel.php
+++ account/model/dbhmodel.php
@@ -2,10 +2,10 @@
 
 class Dbh {
 
-    private $host = "localhost";
-    private $username = "root"; 
-    private $password = "";
-    private $dbname = "career";
+    private $host = "kl28h27lwgjntlbt.chr7pe7iynqr.eu-west-1.rds.amazonaws.com";
+    private $username = "euswoktlhfjaakft"; 
+    private $password = "vhyoh0gvvc7mrp64";
+    private $dbname = "	primary_app_db";
 
 protected function connect(){
 
