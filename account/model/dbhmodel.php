7e688de9b2725b02a09f8ac338ad773930d5e951
Amadou Sarjo Jallow <asj.sarjo@gmail.com>
2020-06-05 01:43:00 +0000

    Update dbhmodel.php

--- account/model/dbhmodel.php
+++ account/model/dbhmodel.php
@@ -1,14 +1,14 @@
 <?php
 
 class Dbh {
-    $url = getenv('mysql://euswoktlhfjaakft:vhyoh0gvvc7mrp64@kl28h27lwgjntlbt.chr7pe7iynqr.eu-west-1.rds.amazonaws.com:3306/primary_app_db');
-    $dbparts = parse_url($url);
-
+    public $url = getenv('mysql://euswoktlhfjaakft:vhyoh0gvvc7mrp64@kl28h27lwgjntlbt.chr7pe7iynqr.eu-west-1.rds.amazonaws.com:3306/primary_app_db');
+    public $dbparts = parse_url($url);
+ var_dump($url);
     private $host = $dbparts['host'];
     private $username = $dbparts['user'];
     private $password = $dbparts['pass'];
     private $dbname = ltrim($dbparts['path'],'/');
-                var_dump($host." ".$username." ".$password." ".$dbname);
+//                 var_dump($host." ".$username." ".$password." ".$dbname);
 //     private $host = "kl28h27lwgjntlbt.chr7pe7iynqr.eu-west-1.rds.amazonaws.com";
 //     private $username = "euswoktlhfjaakft"; 
 //     private $password = "vhyoh0gvvc7mrp64";
