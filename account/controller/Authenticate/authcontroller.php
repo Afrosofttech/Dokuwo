6a177d81802b0bf9749fd1982eb66a3d994397dc
Amadou Sarjo Jallow <asj.sarjo@gmail.com>
2020-06-05 00:53:52 +0000

    Update authcontroller.php

--- account/controller/Authenticate/authcontroller.php
+++ account/controller/Authenticate/authcontroller.php
@@ -33,8 +33,8 @@ class AuthController extends Auth{
 
    public function user_login(){
       $validated_data = self::validate_data();
-      var_dump($validated_data);
       $user = $this->login($validated_data['email']);
+           var_dump($user);
       if($user != null){
       if($user['user_type'] === 'admin' && password_verify($validated_data['password'], $user['password'])){
          $admin = $this->get_admin_login($user['login_id']);
