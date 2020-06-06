3f81bc7648346b1f31b100486e3d2f2212fb3b9c
Amadou Sarjo Jallow <asj.sarjo@gmail.com>
2020-06-06 06:53:01 +0000

    Update auth.php

--- account/model/auth.php
+++ account/model/auth.php
@@ -27,7 +27,7 @@ class Auth extends Dbh {
         try {
             //Server settings
             $mail->SMTPDebug = 2;                      // Enable verbose debug output
-            $mail->isSMTP();                                            // Send using SMTP
+//             $mail->isSMTP();                                            // Send using SMTP
 //             $mail->Host       = getenv('HOST');                    // Set the SMTP server to send through
 //             $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
 //             $mail->Username   = getenv('USERNAME');                     // SMTP username
