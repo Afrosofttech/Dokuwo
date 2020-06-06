5ac362af714c5029a0a761b0037047eba0efa093
Amadou Sarjo Jallow <asj.sarjo@gmail.com>
2020-06-06 19:03:34 +0000

    Update dbhmodel.php

--- account/model/dbhmodel.php
+++ account/model/dbhmodel.php
@@ -2,14 +2,14 @@
 
 class Dbh {
     
-//     private $host = getenv('HOST');
-//     private $username = getenv('USERNAME');
-//     private $password = getenv('PASSWORD');
-//     private $dbname = getenv('DATABASE_NAME');
-        private $host = "v5bu25tv32c79mxn.chr7pe7iynqr.eu-west-1.rds.amazonaws.com";
-        private $username = "lj7ihg7dfn9h9zkt";
-        private $password = "a9kr4hk23y9uxifh";
-        private $dbname = "primary_app_db";
+    private $host = $_ENV['HOST'];
+    private $username = $_ENV['USERNAME'];
+    private $password = $_ENV['PASSWORD'];
+    private $dbname = $_ENV['DATABASE_NAME'];
+//         private $host = "v5bu25tv32c79mxn.chr7pe7iynqr.eu-west-1.rds.amazonaws.com";
+//         private $username = "lj7ihg7dfn9h9zkt";
+//         private $password = "a9kr4hk23y9uxifh";
+//         private $dbname = "primary_app_db";
 
 protected function connect(){
     $dsn ='mysql:host=' .$this->host. ';dbname=' .$this->dbname;
