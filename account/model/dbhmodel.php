029232c7bcd01444918ca0139dc3ea2c16e92731
AMS-JR <asj.sarjo@gmail.com>
2020-06-08 04:55:18 +0000

    rm class props

--- account/model/dbhmodel.php
+++ account/model/dbhmodel.php
@@ -6,18 +6,13 @@ define('PASSWORD',  getenv('PASSWORD'));
 define('DATABASE_NAME', getenv('DATABASE_NAME'));
 class Dbh {
     
-//         private $host = "localhost";
-//         private $username = "root";
-//         private $password = "";
-//         private $dbname = "career";
+    protected function connect(){
+        $dsn ='mysql:host=' .HOST. ';dbname=' .DATABASE_NAME;
+        $pdo = new PDO($dsn,USERNAME,PASSWORD);    
+        $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
-protected function connect(){
-    $dsn ='mysql:host=' .HOST. ';dbname=' .DATABASE_NAME;
-    $pdo = new PDO($dsn,USERNAME,PASSWORD);    
-    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-
-    return $pdo;
-}
+        return $pdo;
+    }
 
 }
 ?>
