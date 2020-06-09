01280af605a8f8b628ae30eb5457bcb020833223
AMS-JR <asj.sarjo@gmail.com>
2020-06-09 20:53:36 +0000

    aws assets

--- account/forgot-pwd.php
+++ account/forgot-pwd.php
@@ -10,22 +10,22 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta name="description" content="This is a job hunting platform that brings together Recruiters, Freelancers and potential employees.">
   <meta name="author" content="Afrika Software Technologies">
-  <link rel="apple-touch-icon" sizes="57x57" href="assets/img/fav/apple-icon-57x57.png">
-  <link rel="apple-touch-icon" sizes="60x60" href="assets/img/fav/apple-icon-60x60.png">
-  <link rel="apple-touch-icon" sizes="72x72" href="assets/img/fav/apple-icon-72x72.png">
-  <link rel="apple-touch-icon" sizes="76x76" href="assets/img/fav/apple-icon-76x76.png">
-  <link rel="apple-touch-icon" sizes="114x114" href="assets/img/fav/apple-icon-114x114.png">
-  <link rel="apple-touch-icon" sizes="120x120" href="assets/img/fav/apple-icon-120x120.png">
-  <link rel="apple-touch-icon" sizes="144x144" href="assets/img/fav/apple-icon-144x144.png">
-  <link rel="apple-touch-icon" sizes="152x152" href="assets/img/fav/apple-icon-152x152.png">
-  <link rel="apple-touch-icon" sizes="180x180" href="assets/img/fav/apple-icon-180x180.png">
-  <link rel="icon" type="image/png" sizes="192x192"  href="assets/img/fav/android-icon-192x192.png">
-  <link rel="icon" type="image/png" sizes="32x32" href="assets/img/fav/favicon-32x32.png">
-  <link rel="icon" type="image/png" sizes="96x96" href="assets/img/fav/favicon-96x96.png">
-  <link rel="icon" type="image/png" sizes="16x16" href="assets/img/fav/favicon-16x16.png">
-  <link rel="manifest" href="assets/img/fav/manifest.json">
+  <link rel="apple-touch-icon" sizes="57x57" href="https://dokuwo-assets.s3.amazonaws.com/assets/img/fav/apple-icon-57x57.png">
+  <link rel="apple-touch-icon" sizes="60x60" href="https://dokuwo-assets.s3.amazonaws.com/assets/img/fav/apple-icon-60x60.png">
+  <link rel="apple-touch-icon" sizes="72x72" href="https://dokuwo-assets.s3.amazonaws.com/assets/img/fav/apple-icon-72x72.png">
+  <link rel="apple-touch-icon" sizes="76x76" href="https://dokuwo-assets.s3.amazonaws.com/assets/img/fav/apple-icon-76x76.png">
+  <link rel="apple-touch-icon" sizes="114x114" href="https://dokuwo-assets.s3.amazonaws.com/assets/img/fav/apple-icon-114x114.png">
+  <link rel="apple-touch-icon" sizes="120x120" href="https://dokuwo-assets.s3.amazonaws.com/assets/img/fav/apple-icon-120x120.png">
+  <link rel="apple-touch-icon" sizes="144x144" href="https://dokuwo-assets.s3.amazonaws.com/assets/img/fav/apple-icon-144x144.png">
+  <link rel="apple-touch-icon" sizes="152x152" href="https://dokuwo-assets.s3.amazonaws.com/assets/img/fav/apple-icon-152x152.png">
+  <link rel="apple-touch-icon" sizes="180x180" href="https://dokuwo-assets.s3.amazonaws.com/assets/img/fav/apple-icon-180x180.png">
+  <link rel="icon" type="image/png" sizes="192x192"  href="https://dokuwo-assets.s3.amazonaws.com/assets/img/fav/android-icon-192x192.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="https://dokuwo-assets.s3.amazonaws.com/assets/img/fav/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="96x96" href="https://dokuwo-assets.s3.amazonaws.com/assets/img/fav/favicon-96x96.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="https://dokuwo-assets.s3.amazonaws.com/assets/img/fav/favicon-16x16.png">
+  <link rel="manifest" href="https://dokuwo-assets.s3.amazonaws.com/assets/img/fav/manifest.json">
   <meta name="msapplication-TileColor" content="#ffffff">
-  <meta name="msapplication-TileImage" content="assets/img/fav/ms-icon-144x144.png">
+  <meta name="msapplication-TileImage" content="https://dokuwo-assets.s3.amazonaws.com/assets/img/fav/ms-icon-144x144.png">
   <meta name="theme-color" content="#ffffff">
 
   <title>Dokuwo - Forgot Password</title>
