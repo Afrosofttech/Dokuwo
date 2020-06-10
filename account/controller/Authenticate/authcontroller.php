2e9a5ab826986642a0ee30069553551c87da570d
AMS-JR <asj.sarjo@gmail.com>
2020-06-10 17:25:39 +0000

    vendor

--- account/controller/Authenticate/authcontroller.php
+++ account/controller/Authenticate/authcontroller.php
@@ -172,7 +172,7 @@ class AuthController extends Auth{
          if(in_array($ext, $valid_extensions)){ 
          $path = $path.strtolower($final_image);
          // move_uploaded_file($tmp,$path);
-         $upload = $s3->upload(BUCKET, $_FILES['logo']['name'], fopen($_FILES['logo']['tmp_name'], 'rb'), 'public-read');
+         $upload = $s3->upload(BUCKET, $final_image, fopen($_FILES['logo']['tmp_name'], 'rb'), 'public-read');
          }else{
             return 'Invalid';  //@ams-> make sure this is also considered as a return value
          }
