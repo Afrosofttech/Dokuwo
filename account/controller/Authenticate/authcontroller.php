e39c2268bf81218b0bbfb307b4034e4884a182d8
AMS-JR <asj.sarjo@gmail.com>
2020-06-11 03:34:33 +0000

    vendor done

--- account/controller/Authenticate/authcontroller.php
+++ account/controller/Authenticate/authcontroller.php
@@ -158,7 +158,6 @@ class AuthController extends Auth{
           'version'  => 'latest',
           'region'   => 'us-east-1',
       ]);
-      global $bucket;
       $company_data = self::validate_company();
       $exist = $this->does_profile_already_exist($company_data['id'],'company');
       if(!$exist){
@@ -172,7 +171,6 @@ class AuthController extends Auth{
          if(in_array($ext, $valid_extensions)){ 
          $content_map = array('jpeg' => 'image/jpeg','jpg' => 'image/jpeg','png' => 'image/png');
          $contentType = $this->content_type($ext,$valid_extensions,$content_map);
-         var_dump($contentType); die;
          // move_uploaded_file($tmp,$path);
          $upload = $s3->putObject([
             'Bucket' => $bucket,
@@ -198,7 +196,7 @@ class AuthController extends Auth{
 // get the image content-type
    public function content_type($ext,$valid_extensions,$content_map){
       $key = array_keys($valid_extensions, $ext);
-      return $content_map[$key[0]];
+      return $content_map[$valid_extensions[$key[0]]];
    }
 //  admin account creation by super admin
    public function create_admin_account(){
