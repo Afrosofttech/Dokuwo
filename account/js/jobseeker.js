494a2871c5c72f144e0a2a7c67954a5ea20188f9
AMS-JR <asj.sarjo@gmail.com>
2020-06-11 12:40:52 +0000

    update fill j details

--- account/js/jobseeker.js
+++ account/js/jobseeker.js
@@ -164,53 +164,53 @@ function loadJobseekerDashboard(){
  let jtopBar = '<nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">'+
       '<ul class="navbar-nav ml-auto">'+
       '<!-- Nav Item - Alerts -->'+
-      '<li class="nav-item dropdown no-arrow mx-1">'+
-        '<a class="nav-link dropdown-toggle" href="#" id="alertsDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">'+
-          '<i class="fas fa-bell fa-fw"></i>'+
-          '<!-- Counter - Alerts -->'+
-          ' <span class="badge badge-danger badge-counter">3+</span>'+
-        '</a>'+
-        '<!-- Dropdown - Alerts -->'+
-        ' <div class="dropdown-list dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="alertsDropdown">'+
-          '<h6 class="dropdown-header">'+
-            ' Alerts Center'+
-          '</h6>'+
-          '<a class="dropdown-item d-flex align-items-center" href="#">'+
-            '<div class="mr-3">'+
-              ' <div class="icon-circle bg-primary">'+
-                '<i class="fas fa-file-alt text-white"></i>'+
-              '</div>'+
-            '</div>'+
-            '<div>'+
-              ' <div class="small text-gray-500">December 12, 2019</div>'+
-              '<span class="font-weight-bold">A new monthly report is ready to download!</span>'+
-            ' </div>'+
-          '</a>'+
-          '<a class="dropdown-item d-flex align-items-center" href="#">'+
-            '<div class="mr-3">'+
-              '<div class="icon-circle bg-success">'+
-                '<i class="fas fa-donate text-white"></i>'+
-              '</div>'+
-            '</div>'+
-            '<div>'+
-              '<div class="small text-gray-500">December 7, 2019</div>'+
-              '$290.29 has been deposited into your account!'+
-            '</div>'+
-          '</a>'+
-          '<a class="dropdown-item d-flex align-items-center" href="#">'+
-            '<div class="mr-3">'+
-              '<div class="icon-circle bg-warning">'+
-                '<i class="fas fa-exclamation-triangle text-white"></i>'+
-              '</div>'+
-            ' </div>'+
-            '<div>'+
-              '<div class="small text-gray-500">December 2, 2019</div>'+
-              ' Spending Alert: We\'ve noticed unusually high spending for your account.'+
-            '</div>'+
-          ' </a>'+
-          '<a class="dropdown-item text-center small text-gray-500" href="#">Show All Alerts</a>'+
-        '</div>'+
-      ' </li>'+
+      // '<li class="nav-item dropdown no-arrow mx-1">'+
+      //   '<a class="nav-link dropdown-toggle" href="#" id="alertsDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">'+
+      //     '<i class="fas fa-bell fa-fw"></i>'+
+      //     '<!-- Counter - Alerts -->'+
+      //     ' <span class="badge badge-danger badge-counter">3+</span>'+
+      //   '</a>'+
+      //   '<!-- Dropdown - Alerts -->'+
+      //   ' <div class="dropdown-list dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="alertsDropdown">'+
+      //     '<h6 class="dropdown-header">'+
+      //       ' Alerts Center'+
+      //     '</h6>'+
+      //     '<a class="dropdown-item d-flex align-items-center" href="#">'+
+      //       '<div class="mr-3">'+
+      //         ' <div class="icon-circle bg-primary">'+
+      //           '<i class="fas fa-file-alt text-white"></i>'+
+      //         '</div>'+
+      //       '</div>'+
+      //       '<div>'+
+      //         ' <div class="small text-gray-500">December 12, 2019</div>'+
+      //         '<span class="font-weight-bold">A new monthly report is ready to download!</span>'+
+      //       ' </div>'+
+      //     '</a>'+
+      //     '<a class="dropdown-item d-flex align-items-center" href="#">'+
+      //       '<div class="mr-3">'+
+      //         '<div class="icon-circle bg-success">'+
+      //           '<i class="fas fa-donate text-white"></i>'+
+      //         '</div>'+
+      //       '</div>'+
+      //       '<div>'+
+      //         '<div class="small text-gray-500">December 7, 2019</div>'+
+      //         '$290.29 has been deposited into your account!'+
+      //       '</div>'+
+      //     '</a>'+
+      //     '<a class="dropdown-item d-flex align-items-center" href="#">'+
+      //       '<div class="mr-3">'+
+      //         '<div class="icon-circle bg-warning">'+
+      //           '<i class="fas fa-exclamation-triangle text-white"></i>'+
+      //         '</div>'+
+      //       ' </div>'+
+      //       '<div>'+
+      //         '<div class="small text-gray-500">December 2, 2019</div>'+
+      //         ' Spending Alert: We\'ve noticed unusually high spending for your account.'+
+      //       '</div>'+
+      //     ' </a>'+
+      //     '<a class="dropdown-item text-center small text-gray-500" href="#">Show All Alerts</a>'+
+      //   '</div>'+
+      // ' </li>'+
         '<li class="nav-item dropdown no-arrow mx-1">'+
           '<a class="nav-link dropdown-toggle" href="#" id="messagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">'+
             '<i class="fas fa-envelope fa-fw"></i>'+
@@ -2673,7 +2673,7 @@ let temp='<div class="content-wrapper">'+
             }else if( response == "Invalid Image"){
                 swal('Invalid Image type!','You can only upload png, jpeg, or jpg','error','Cool');
             }else if( response == "Invalid CV"){
-              swal('Invalid CV type!','You can only upload jpeg, jpg, png, pdf, doc or ppt','error','Cool');
+              swal('Invalid CV type!','You can only upload jpeg, jpg, png, pdf, doc, docx, txt or zip','error','Cool');
           }
           },
           error: function(err){
