2d9f68f8aaaa41602653d4b51550efdd49e198fa
AMS-JR <asj.sarjo@gmail.com>
2020-06-12 15:35:55 +0000

    minor changes

--- account/model/auth.php
+++ account/model/auth.php
@@ -133,12 +133,11 @@ class Auth extends Dbh {
     }
 
     public function jobseeker_account($login_id,$fname,$lname,$fullname,$phone,$skills,$edulevel,$adr,$dob,$country,$category,$interest,$tag_line,$description,$image,$cv){
-        if(($image == null || $image == 'null') && ($cv == null || $cv == 'null')){ $sql = "INSERT INTO job_seeker (login_id,fname,lname,fullname,phone,skills,category,interest,seeksJob,tag_line,education_level,address,dob,country,description,featured) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)"; var_dump('BOTH');}
-        else if($image == null || $image == 'null'){ $sql = "INSERT INTO job_seeker (login_id,fname,lname,fullname,phone,skills,category,interest,seeksJob,tag_line,education_level,address,dob,country,cv,description,featured) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)"; var_dump('IMAGE');}
-        else if($cv == null || $cv == 'null'){ $sql = "INSERT INTO job_seeker (login_id,fname,lname,fullname,phone,skills,category,interest,seeksJob,tag_line,education_level,address,dob,country,image,description,featured) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)"; var_dump('CV');}
+        if(($image == null || $image == 'null') && ($cv == null || $cv == 'null')) $sql = "INSERT INTO job_seeker (login_id,fname,lname,fullname,phone,skills,category,interest,seeksJob,tag_line,education_level,address,dob,country,description,featured) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+        else if($image == null || $image == 'null') $sql = "INSERT INTO job_seeker (login_id,fname,lname,fullname,phone,skills,category,interest,seeksJob,tag_line,education_level,address,dob,country,cv,description,featured) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+        else if($cv == null || $cv == 'null') $sql = "INSERT INTO job_seeker (login_id,fname,lname,fullname,phone,skills,category,interest,seeksJob,tag_line,education_level,address,dob,country,image,description,featured) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
         else $sql = "INSERT INTO job_seeker (login_id,fname,lname,fullname,phone,skills,category,interest,seeksJob,tag_line,education_level,address,dob,country,image,cv,description,featured) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
         $stmt = $this->connect()->prepare($sql);
-        var_dump('AMADOU SARJO');
         if(($image == null || $image == 'null') && ($cv == null || $cv == 'null')) $stmt->execute([$login_id,$fname,$lname,$fullname,$phone,$skills,$category,$interest,'yes',$tag_line,$edulevel,$adr,$dob,$country,$description,0]);
         else if($image == null || $image == 'null') $stmt->execute([$login_id,$fname,$lname,$fullname,$phone,$skills,$category,$interest,'yes',$tag_line,$edulevel,$adr,$dob,$country,$cv,$description,0]);
         else if($cv == null || $cv == 'null') $stmt->execute([$login_id,$fname,$lname,$fullname,$phone,$skills,$category,$interest,'yes',$tag_line,$edulevel,$adr,$dob,$country,$image,$description,0]);
