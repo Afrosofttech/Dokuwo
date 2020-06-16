3f027072340b6f6b51a3f13703781b9cfbb81054
AMS-JR <asj.sarjo@gmail.com>
2020-06-16 17:10:24 +0000

    admin added...

---
+++ account/view/Admin/adminmessagesview.php
@@ -0,0 +1,41 @@
+<?php
+
+include_once 'model/adminmodel.php';
+
+class AdminMessagesView extends Admin{
+    public function no_of_new_messages($recipient_id)
+    {
+        $noOfNewRecievedMessages = $this->get_no_of_new_messages($recipient_id);
+        return $noOfNewRecievedMessages;
+    }
+    public function all_inbox_messages($login_id)
+    { 
+        $allInboxMessages = $this->get_all_inbox_messages($login_id);
+        return $allInboxMessages;
+    }
+    public function read_messages($recipient_id)
+    {
+        $res = $this->get_read_messages($recipient_id);
+        return $res;
+    }
+    public function get_message($msg_id)
+    {
+        $allInboxMessages = $this->get_this_message($msg_id);
+        return $allInboxMessages;
+    }
+    public function new_unread_messages($recipient_id)
+    {
+        $allUnreadMessages = $this->get_new_unread_messages($recipient_id);
+        return $allUnreadMessages;
+    }
+    public function all_sent_messages($creator_id)
+    {
+        $allSentMessages = $this->get_all_sent_messages($creator_id);
+        return $allSentMessages;
+    }
+    public function retreive_all_users()
+    {
+        $listOfAllUsers =$this->get_all_users();
+        return $listOfAllUsers;  
+    }
+}
