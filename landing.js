589c782e874f317ae099a6e8f5e5acae0286857c
techjobis <biranjobe69@gmail.com>
2020-06-21 16:42:20 +0100

    fixing decoding blog content

--- landing.js
+++ landing.js
@@ -2251,7 +2251,7 @@ else{
      
       let blog_details = '';
       $.ajax({
-        method: "POST",
+        method: "GET",
         url: "account/get.php/company/get_blog_details",
         dataType: "json",
         data: {'blog_id':blog_id},
